1a30555b308e84ad2237e731b1bcb0a760d54561
Dan Morgan <ltdan681@gmail.com>
2020-03-29 20:44:18 -0400

    Updates

---
+++ precache-manifest.ac48577bcee0fbba797b3076e11b648e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7efe78326c8e233b5ecc705bd25b6ef3",
+    "url": "/drum-kit/index.html"
+  },
+  {
+    "revision": "4182f3fe1d141a42a0a1",
+    "url": "/drum-kit/static/css/main.789a23b7.chunk.css"
+  },
+  {
+    "revision": "a503ded9c5ec53a32e3a",
+    "url": "/drum-kit/static/js/2.d566456e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-kit/static/js/2.d566456e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4182f3fe1d141a42a0a1",
+    "url": "/drum-kit/static/js/main.b908b612.chunk.js"
+  },
+  {
+    "revision": "41db95ef2fb623aeddf5",
+    "url": "/drum-kit/static/js/runtime-main.4525d8e7.js"
+  }
+]);
